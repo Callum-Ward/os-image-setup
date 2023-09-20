4fdf67cf4d2193487ffca0e95789a1708e1bcf69
Callum-Ward <callumward2000@gmail.com>
2023-09-20 12:09:30 +0100

    Implement command line processing

--- dev.py
+++ dev.py
@@ -2,6 +2,13 @@ import json
 import sys
 import subprocess
 
+def run_command(command, display_output=False):
+    if display_output:
+        result = subprocess.run(command, capture_output=True, text=True)
+        print(result.stdout)
+    else:   
+        subprocess.run(command)
+
 
 if __name__ == "__main__":
 
@@ -17,6 +24,16 @@ if __name__ == "__main__":
         name = config['os_image']['name']
         print(f"os_image: {name}")
     
+    # Example usage
+    command = input("Enter a command: ").split()
+
+    diplay_output = input("Display command output? (y/n): ")
+    if diplay_output == 'y':
+        run_command(command, True)
+    else:
+        run_command(command)
+        
+
 
 
 
