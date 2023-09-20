4608deb53e54ee792385fb16d93c0dd793efcdcd
Callum-Ward <callumward2000@gmail.com>
2023-09-20 16:11:17 +0100

    Implement logging

--- dev.py
+++ dev.py
@@ -1,14 +1,27 @@
 import json
 import sys
 import subprocess
+import os
+import logging
 
-def run_command(command, display_output=False):
-    if display_output:
-        result = subprocess.run(command, capture_output=True, text=True)
-        print(result.stdout)
-    else:   
-        subprocess.run(command)
 
+def run_command(command, logging, display_output):
+
+    try:
+        result = subprocess.run(command, shell=True, capture_output=True, text=True)    
+    except subprocess.CalledProcessError as e:
+        # Handle the error
+        print(e.stderr)
+        logging.error(f" Program return code: {e.returncode}\n{e.stderr}")
+        sys.exit(1)
+        
+    if result.returncode:
+        if display_output: print(f"'{command}' FAILED")
+        logging.error(f" command '{command}' return code: {result.returncode}\n{result.stderr}")
+    else:
+        if display_output: print(f"'{command}' EXECUTED")
+        logging.info(f" command '{command}' returned: \n{result.stdout}")
+    
 
 if __name__ == "__main__":
 
@@ -23,15 +36,19 @@ if __name__ == "__main__":
     if 'name' in config['os_image']:
         name = config['os_image']['name']
         print(f"os_image: {name}")
-    
-    # Example usage
-    command = input("Enter a command: ").split()
-
-    diplay_output = input("Display command output? (y/n): ")
-    if diplay_output == 'y':
-        run_command(command, True)
     else:
-        run_command(command)
+        print("No name provided for os_image")
+        os._exit(1)
+
+    logging.basicConfig(filename="command_log.txt", filemode="w", level=logging.INFO)
+
+    command = input("Enter a command: ")
+
+    diplay_output = input("Display command output? (y/n): ").lower()
+    if diplay_output == 'y':
+        run_command(command, logging, True)
+    else:
+        run_command(command, logging, False)
         
 
 
